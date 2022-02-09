12fd5e851c080b9dc8fb89d244f126fa56f7c844
choyuj1n <dbwlsdl8534@naver.com>
2022-02-09 11:13:37 +0900

    test

---
+++ 연습문제2.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.random.normal(5,3,1000)
+y = np.random.normal(2,2,1000)
+
+plt.scatter(x, y, color='pink', alpha=o.5)
+
+plt.title('2021 Math Score Distribution')
+plt.xlabel('score')
+plt.ylabel('student')
+plt.righttitle
